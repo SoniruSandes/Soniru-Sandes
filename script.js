f1510ab600671f81c33af792a76734a28476e957
SoniruSandes <123316284+SoniruSandes@users.noreply.github.com>
2024-08-18 00:42:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.getElementById('myButton').addEventListener('click', function() {
     alert('Button Clicked!');
 });
+
+document.getElementById('themeSwitcher').addEventListener('click', function() {
+    document.body.classList.toggle('dark');
+    this.innerHTML = document.body.classList.contains('dark') ? '&#9788;' : '&#9728;';
+});
