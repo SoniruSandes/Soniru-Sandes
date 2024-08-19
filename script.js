15a30cc09652a9aa7c3ee9732f65b6f2991c5269
SoniruSandes <123316284+SoniruSandes@users.noreply.github.com>
2024-08-19 10:20:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('myButton').addEventListener('click', function() {
-    alert('Button Clicked!');
+    alert('https://youtube.com/@Gaming-Quest-');
 });
 
 const themeSwitcher = document.querySelector('.theme-switcher');
@@ -8,3 +8,13 @@ themeSwitcher.addEventListener('click', () => {
     document.body.classList.toggle('dark');
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const contactLink = document.querySelector('a[href="#contact"]');
+    const contactForm = document.getElementById('contact-form');
+
+    contactLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        contactForm.style.display = 'block';
+        window.scrollTo(0, contactForm.offsetTop);
+    });
+});
